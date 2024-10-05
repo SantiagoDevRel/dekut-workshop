f2ea6ba62494d522ab11324d66383865f037b7b7
santiagodevrel <santiago@chainsafe.io>
2024-10-05 16:31:17 +0300

    updated homepage packagejson

--- src/abi.mjs
+++
@@ -1,8 +0,0 @@
-const ABI = [
-  { inputs: [{ internalType: "uint256", name: "_startingPoint", type: "uint256" }], stateMutability: "nonpayable", type: "constructor" },
-  { inputs: [], name: "dicreaseCounter", outputs: [], stateMutability: "nonpayable", type: "function" },
-  { inputs: [], name: "getCounter", outputs: [{ internalType: "uint256", name: "", type: "uint256" }], stateMutability: "view", type: "function" },
-  { inputs: [], name: "increaseCounter", outputs: [], stateMutability: "nonpayable", type: "function" },
-];
-
-export default ABI;
