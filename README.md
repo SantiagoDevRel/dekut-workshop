08a0d9e114849fce02c2a7793d2bb26a0820ddfc
santiagodevrel <santiago@chainsafe.io>
2024-10-05 16:13:46 +0300

    update app

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # SEPOLIA CONTRACT
 
 ## https://sepolia.etherscan.io/address/0x3bf2990f3d54b9e20dbc5234a5d761a0f08c4308#code
+
+# steps
+
+1. npm install web3 and import
+2. initialize web3 object
+3. import ABI of the contract
+4. initialize contract
+5. interact with reading functions
+
+6. connect accounts
+7. interact with writing functions
--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+//1. import Web3 module
+import { Web3 } from "web3";
+import { useState } from "react";
+
+const ADDRESS = "0x3BF2990f3D54B9e20DBc5234A5d761a0f08C4308";
+const ABI = [
+  { inputs: [{ internalType: "uint256", name: "_startingPoint", type: "uint256" }], stateMutability: "nonpayable", type: "constructor" },
+  { inputs: [], name: "dicreaseCounter", outputs: [], stateMutability: "nonpayable", type: "function" },
+  { inputs: [], name: "getCounter", outputs: [{ internalType: "uint256", name: "", type: "uint256" }], stateMutability: "view", type: "function" },
+  { inputs: [], name: "increaseCounter", outputs: [], stateMutability: "nonpayable", type: "function" },
+];
 
 function App() {
+  const [counter, setCounter] = useState("none");
+
+  //2. initialize the web3 object with injected provider
+  const web3 = new Web3(window.ethereum);
+
+  //3. initialize the contract ABI + ADDRESS
+  const myContract = new web3.eth.Contract(ABI, ADDRESS);
+
+  //4. interact with reading functions
+  async function callContract() {
+    const result = await myContract.methods.getCounter().call();
+
+    // render front end
+    setCounter(result.toString());
+  }
+
+  //5. connect accounts and writing functions
+  async function increaseCounter() {}
+
+  async function dicreaseCounter() {}
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={callContract}>Get current counter</button>
+        <button onClick={increaseCounter}>Increase counter</button>
+        <button onClick={dicreaseCounter}>Dicrease counter</button>
+        <p>Counter: {counter}</p>
       </header>
     </div>
   );
